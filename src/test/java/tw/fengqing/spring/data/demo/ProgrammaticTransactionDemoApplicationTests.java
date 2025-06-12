e26dac78db182471a10001a0d174630b7684a995
chukuolung <chukuo99@gmail.com>
2025-06-12 15:31:08 +0800

    init

---
+++ src/test/java/tw/fengqing/spring/data/demo/ProgrammaticTransactionDemoApplicationTests.java
@@ -0,0 +1,14 @@
+package tw.fengqing.spring.data.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProgrammaticTransactionDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
